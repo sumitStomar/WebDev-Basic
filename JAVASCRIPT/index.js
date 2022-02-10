4bc4bca11e27a811255cf9213eeedaca9c6be5f2
Sumit Singh Tomar <tomarsumit570@gmail.com>
2022-02-10 18:53:30 +0530

    Basic Properties Javascript

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -3,12 +3,18 @@
 
 
 // variable declaration
-var a; //int string float ?
+var a; //int string float ? // humein batana nhi hota js mein ye kya hota hai 
+
+// typeof operator -> it tells us the type of data stored in a vaiable
 
 a = 10;
 console.log(typeof a );
 console.log(a);
 
-a = "hello";
+a = "hello"; //re initialize of variable 'a'
 console.log(typeof a);
 console.log(a);
+
+a= true;
+console.log(a);
+console.log(typeof a);
