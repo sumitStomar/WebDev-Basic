54fbbcd0c0c63fd9d0754419efd7f92bf0faccbc
Sumit Singh Tomar <tomarsumit570@gmail.com>
2022-02-10 18:49:00 +0530

    Javascript

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,14 @@
+//starting JS -> JavaScript
+//var let const
+
+
+// variable declaration
+var a; //int string float ?
+
+a = 10;
+console.log(typeof a );
+console.log(a);
+
+a = "hello";
+console.log(typeof a);
+console.log(a);
