31ae1ac7819ad93b43051fa88224d48a3dacca25
Sumit Singh Tomar <tomarsumit570@gmail.com>
2022-02-12 20:09:09 +0530

    Let Variable

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -26,30 +26,55 @@
 //  console.log(typeof a);
 
 //Number
-var num = 10;
-console.log(num);
-var float = 2.4;
-console.log(float);
+// var num = 10;
+// console.log(num);
+// var float = 2.4;
+// console.log(float);
 
-// Boolean
-var t = true;
-var f = false;
-console.log(t);
-console.log(f);
+// // Boolean
+// var t = true;
+// var f = false;
+// console.log(t);
+// console.log(f);
 
-//String
-//    ' '      ,      " "     ,   ` ` 
-//single quotes, double quotes, backtick
-var str = 'a';
-console.log(typeof str);
-str = "How you doin ?\n i am fine";
-//backslash n("\n")
-console.log(str);
+// //String
+// //    ' '      ,      " "     ,   ` ` 
+// //single quotes, double quotes, backtick
+// var str = 'a';
+// console.log(typeof str);
+// str = "How you doin ?\n i am fine";
+// //backslash n("\n")
+// console.log(str);
 
-var b = `enjoying the course
-what are u doing`;
-console.log(b);
+// var b = `enjoying the course
+// what are u doing`;
+// console.log(b);
 
-var num = 4500;
-// ${}
-console.log(`half of ${num} is ${num/2}`);
+// var num = 4500;
+// // ${}
+// console.log(`half of ${num} is ${num/2}`);
+
+
+
+//VAR HAS SOME PROBLEM
+
+// 1) redeclaration is allowed
+var r = "hello";
+console.log(r)
+
+var r = 100;
+console.log(r);
+
+//let KeyWord is Used
+let l = 100 ;  
+console.log(l);
+
+// overcome karne ke liye
+
+//  let l = '200'  //SyntaxError: Identifier 'l' has already been declared
+// console.log(l);
+
+// we can reinitialize a variable but we cannot redeclare it
+l  = 300;
+console.log(l);
+
