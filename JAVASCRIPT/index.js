920bce994da4f8d87314ddcef1311650c56e600b
Sumit Singh Tomar <tomarsumit570@gmail.com>
2022-02-12 19:08:28 +0530

    Basic JS

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,20 +1,46 @@
-//starting JS -> JavaScript
-//var let const
+// //starting JS -> JavaScript
+// //var let const
 
 
-// variable declaration
-var a; //int string float ? // humein batana nhi hota js mein ye kya hota hai 
+// // variable declaration
+// var a; //int string float ? // humein batana nhi hota js mein ye kya hota hai 
 
-// typeof operator -> it tells us the type of data stored in a vaiable
 
-a = 10;
-console.log(typeof a );
-console.log(a);
 
-a = "hello"; //re initialize of variable 'a'
-console.log(typeof a);
-console.log(a);
+// a = 10;
+// console.log(typeof a );
+// console.log(a);
 
-a= true;
-console.log(a);
-console.log(typeof a);
+// a = "hello"; //re initialize of variable 'a'
+// console.log(typeof a);
+// console.log(a);
+
+// // typeof operator -> it tells us the type of data stored in a vaiable
+
+// a= true;
+// console.log(a);
+// console.log(typeof a);
+
+//  a = null;
+//  console.log(a);
+//  console.log(typeof a);
+
+//Number
+var num = 10;
+console.log(num);
+var float = 2.4;
+console.log(float);
+
+// Boolean
+var t = true;
+var f = false;
+console.log(t);
+console.log(f);
+
+//String
+//    ' '      ,      " "     ,    `` 
+//single quotes, double quotes, backtick
+var str = 'a';
+console.log(typeof str);
+str = "How you doin ?"
+console.log(str);
